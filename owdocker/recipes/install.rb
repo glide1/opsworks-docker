ce2d9c02ec2d1444be2e8ed76336bd632a932750
Min Han <glide1@gmail.com>
2015-07-01 23:45:56 -0400

    updated with simpler install

--- owdocker/recipes/install.rb
+++ owdocker/recipes/install.rb
@@ -1,18 +1,12 @@
-include_recipe 'apt'
-
-apt_repository "docker" do
-  uri "https://get.docker.com/ubuntu"
-  distribution "docker"
-  components ["main"]
-  keyserver "hkp://keyserver.ubuntu.com:80"
-  key "36A1D7869245C8950F966E92D8576A8BA88D21E9"
-  not_if { File.exists?("/etc/apt/sources.list.d/docker.list") }
-end
-
-# Install Docker latest version
-package "docker" do
-  package_name "lxc-docker"
-  action :install
+case node[:platform]
+when "ubuntu","debian"
+  package "docker.io" do
+    action :install
+  end
+when 'centos','redhat','fedora','amazon'
+  package "docker" do
+    action :install
+  end
 end
 
 service "docker" do
